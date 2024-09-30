f9913b606641cc64981c97d3afcacb78121b1c47
rosa-skn <rosa.sokun@epitech.digital>
2024-09-30 16:23:56 +0200

    Create ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,6 @@
+displayThisText('The variable value is ' + "'"+ input_var+ "'." );
+displayThisText('Its type is' +" '" +typeof input_var+ "'.")
+if (input_var== 42){
+displayThisText('It is the meaning of life.')
+
+}
