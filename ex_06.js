a89dace239cefae9c554d6277716046f23e68a8a
rosa-skn <rosa.sokun@epitech.digital>
2024-09-30 14:25:34 +0200

    Create  ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,7 @@
+if (bananasCount == 0) {
+displayThisText("Oh no, there is no banana.");
+} else if (bananasCount < 0) {
+displayThisText("Do I owe you bananas?");
+} else {
+displayThisText("Yummy!");
+}
