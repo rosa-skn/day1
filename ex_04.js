d98a66b6937944a5344caf459727972e58609e2e
rosa-skn <rosa.sokun@epitech.digital>
2024-09-30 12:15:15 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof undefined);
+displayThisText(typeof true);
+displayThisText(typeof 42);
+displayThisText(typeof 123n);
+displayThisText(typeof Symbol());
+displayThisText(typeof[1,2,3,4]);
+displayThisText(typeof function(){});
